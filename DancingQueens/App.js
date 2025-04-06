63ca185e3042eddc0c815bd59a666c752ceeef22
Ruby Friedman <rubyfriedman@Rubys-MacBook-Air-2.local>
2025-04-06 14:52:21 -0400

    visualized! So far we have a header! issue-#7

--- DancingQueens/App.js
+++ DancingQueens/App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}> Feeling <Text style={styles.blueText}>Blue?</Text> 
+      Hire Our Crew! We'll Dance For You! 💃 🕺 ✨</Text>
       <StatusBar style="auto" />
     </View>
+  
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20, // Allows smooth scrolling effect
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D0F1BF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+    color: '#483D03',
+  },
+
 });

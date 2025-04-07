a6de4c6dd243da6ea039ae0ab6777b3c8a5ea11b
Ruby Friedman <rubyfriedman@Rubys-MacBook-Air-2.local>
2025-04-06 21:37:36 -0400

    home page complete issue-#20

--- DancingQueens/App.js
+++ DancingQueens/App.js
@@ -1,17 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Button, Linking, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
     <View style={styles.container}>
-      <Text style={styles.heading}> Feeling <Text style={styles.blueText}>Blue? </Text> 
+      <Text style={styles.heading1}> Feeling <Text style={styles.blueText}>Blue? </Text> 
       Hire Our Crew! We'll Dance For You! 💃 🕺 ✨</Text>
 
       <Image 
-        source={require('./assets/team.jpeg')} // Path to your local image
+        source={require('./assets/team.jpeg')} // Path to image
         style={styles.image}
+        accessible={true}
+        accessibilityLabel="This is a photo of our dancing team!"
       />
+      <Text style={styles.h2}> Our Mission </Text>
+      <Text style={styles.p}>Sometimes, people are sad, and that sucks a lot. But do you know what
+        doesn't suck? Dancing! That's why we created the Dancing Queens (and we
+        are not even 17!) Essentially, you pay us the very very low cheap and
+        affordable rate of 400 dollars per hour to dance for you until you feel
+        cheered up again! We do not offer financial aid for our services because
+        of how super cheap they are. </Text>
+        
+        <Text style={styles.h2}> Book Our Services!</Text>
+        <Text style={styles.p}> Create or log in to an account to get started on booking a dance service! </Text>
+
+        <Text style={styles.small}> This site was designed and published as part of the COMP 333 Software Engineering class at Wesleyan University. This is an exercise.
+          However, if you think this is a good business venture please do let us know and we will maybe make it happen! Peace and love.</Text>
+        <Text style={styles.small}> Photo credit to Sebastian Zimmeck</Text>
+        <Pressable onPress={() => Linking.openURL('https://www.linkedin.com/in/sebastianzimmeck')}>
+  <Text style={{ color: 'blue', textDecorationLine: 'underline' }}>
+    View LinkedIn here
+  </Text>
+</Pressable>
+
 
       <StatusBar style="auto" />
     </View>
@@ -36,11 +58,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  heading: {
+  heading1: {
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
     marginBottom: 20,
+    margin:'5%',
+    flexWrap: 'wrap',
     color: '#483D03',
   },
 
@@ -50,9 +74,39 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    width: 200, // Set the width of the image
-    height: 200, // Set the height of the image
-    resizeMode: 'contain', // Ensures the image maintains aspect ratio
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    margin: '2%',
+    marginBottom: 10,
+  },
+  h2: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 5,
+    margin:'2%',
+    flexWrap: 'wrap',
+    color: '#483D03',
+  },
+  p: {
+    color: '#646536',
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+    margin:'3%',
+    flexWrap: 'wrap',
+    
+  },
+  small: {
+    color: '#646536',
+    fontSize: 12,
+    textAlign: 'center',
+    marginBottom: 20,
+    margin:'3%',
+    flexWrap: 'wrap',
+
   }
 
 });

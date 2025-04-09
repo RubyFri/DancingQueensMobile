851ab83b5225d0b1b4cd031086ffd54f34e1ec9d
Yenta2025 <143851183+Yenta2025@users.noreply.github.com>
2025-04-09 01:38:31 -0400

    fix api bookings listings

--- RESTAPI/Controller/Api/Authentication.php
+++ RESTAPI/Controller/Api/Authentication.php
@@ -1,9 +1,20 @@
 
 <?php
-function authenticate() {
-  if (!isset($_SESSION['user'])) {
-    http_response_code(401);
-    echo json_encode(['error' => 'You must be logged in to access this resource']);
-    exit;
-  }
+// session_start();
+
+function authenticate()
+{
+    // Start the session if it's not started yet
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    // Debugging: Print session data
+    var_dump($_SESSION);
+
+    if (!isset($_SESSION['user'])) {
+        http_response_code(401);
+        echo json_encode(['error' => 'You must be logged in to access this resource']);
+        exit;
+    }
 }
--- RESTAPI/index.php
+++ RESTAPI/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // Allow all origins (for testing purposes)
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:8080");
 // Allow specific methods (GET, POST, DELETE, PUT, OPTIONS)
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 // Allow specific headers (you can include more headers as needed)
